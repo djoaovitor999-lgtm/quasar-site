3be6ae31f8784e1f325c3fa57694d6e0572db607
Duartejv <jvestudo09649@gmail.com>
2026-02-01 09:46:58 -0300

    mod

--- src/components/quasar/QuasarHero.tsx
+++ src/components/quasar/QuasarHero.tsx
@@ -35,7 +35,7 @@ const QuasarHero = () => {
             <img
               src={quasarLogo}
               alt="Quasar"
-              className="inline-block h-[1.5em] w-auto align-baseline drop-shadow-2xl transform translate-y-4 md:translate-y-9"
+              className="inline-block h-[1.5em] w-auto align-baseline drop-shadow-2xl transform translate-y-4 md:translate-y-10"
             />
             {t.hero.TitleSuffix}
           </span>
