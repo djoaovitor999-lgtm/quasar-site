41308c65fc9cf32b3d51e5fd3205dc1c957ffc1a
Duartejv <jvestudo09649@gmail.com>
2026-02-02 07:50:21 -0300

    mod

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -42,8 +42,8 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      {/* Botão ajustado: Discreto (sem fundo) mas com área de clique generosa (p-2) */}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-full p-2 opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+      {/* Botão ajustado: Removemos o background no PC com md:data-[state=open]:bg-transparent */}
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-full p-2 opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground md:data-[state=open]:bg-transparent">
         <X className="h-5 w-5" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
